31fd7947d13faf811cab8460257d1e0c883f59bb
Tochiskool <mrchristian79@yahoo.com>
2022-01-19 22:09:09 +0100

    Files initiated

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ function writePassword() {
 
 }
 
+//Password generator
+var generatePassword = function () {
+  var passwordLength = parseInt(prompt("Choose password length please"));
+  console.log(passwordLength);
+  var lowerCase = confirm("Do you want lowercase characters?");
+  var upperCase = confirm("Do you want upercase characters?");
+  var specialCase = confirm("Do you want special characters?");
+  var nums =confirm("Do you want numbers?")
+ 
+  var numss = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var capLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var specialCharaters = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":",]
+  var container;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
